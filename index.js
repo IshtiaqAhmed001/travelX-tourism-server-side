df5c53a08869587171eb944db64e87fe00bdcb96
IshtiaqAhmed001 <ishtiaq.wasi@gmail.com>
2021-10-29 02:05:54 +0600

    implement GET API

--- index.js
+++ index.js
@@ -1,8 +1,49 @@
-const services = [
-    {
-        name: 'Discover Costarica',
-        price: '$1870',
-        destination: 'Costarica,Panama',
-        img: ''
+const express = require('express');
+const { MongoClient } = require("mongodb");
+const ObjectId = require('mongodb').ObjectId;
+const cors = require('cors');
+require('dotenv').config();
+
+
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+// middleware 
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oteh7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+async function run() {
+    try {
+        await client.connect();
+
+        const database = client.db("travelX");
+        const packageCollection = database.collection("packages");
+
+        // GET API 
+        app.get('/packages', async (req, res) => {
+            const cursor = packageCollection.find({});
+            const packages = await cursor.toArray();
+            res.send(packages);
+        })
     }
-]
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir)
+
+
+
+app.get('/', (req, res) => {
+    res.send('travelX server running');
+});
+
+app.listen(port, () => {
+    console.log('listening from port', port);
+});
