6b350520cd11d5450014e75aa5457cf7b838cfd6
IshtiaqAhmed001 <ishtiaq.wasi@gmail.com>
2021-10-30 21:16:16 +0600

    cancel/delete order implemented

--- index.js
+++ index.js
@@ -53,6 +53,14 @@ async function run() {
             const result = await packageCollection.insertOne(newPackage);
             // console.log(result);
             res.send(result);
+        });
+
+        // DELETE API to delete/cancel booking
+        app.delete('/bookings/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await orderCollection.deleteOne(query);
+            res.json(result);
         })
 
     }
